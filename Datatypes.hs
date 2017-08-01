71648bd5871b6c8d64412baf38288a1c9f5eb154
Satyendra-IITR <satyendrabanjare99@gmail.com>
2017-08-01 15:31:41 +0530

    --

--- Datatypes.hs
+++ Datatypes.hs
@@ -1,1 +1,9 @@
-module 
+module  Datatypes where
+
+type clientname = String
+type channelname = String
+
+data Message = Notice String
+               | Tell clientname String
+               | Broadcast channelname clientname String
+               | Command String
