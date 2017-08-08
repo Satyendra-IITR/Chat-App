49639ca179a01d74f8701cdb85ee71be1fa4504b
Satyendra Banjare <satyendrabanjare99@gmail.com>
2017-08-08 23:25:45 +0530

    Update Main.hs

---
+++ Main.hs
@@ -0,0 +1,28 @@
+module Main where
+
+import Control.Concurrent (forkFinally)
+import Control.Monad      (forever)
+import Network            (PortID(..), accept, listenOn, withSocketsDo)
+import System.IO          (hClose)
+import Text.Printf        (printf)
+
+import Server             (handleClient, newServer)
+
+main :: IO ()
+main = withSocketsDo $ do
+    server <- newServer
+    sock <- listenOn (PortNumber (fromIntegral port))
+    _ <- printf "Server Created on Port :  %d\n" port
+
+    forever $ do
+        (handle, host, port') <- accept sock
+        _ <- printf "Accepted connection from %s: %s\n" host (show port')
+        forkFinally (ServeClient handle server) (\_ -> hClose handle)
+
+
+--make connection via ffi to admin.c and get port number and default channelname  
+
+
+
+port :: Int
+port = -- variable portnumber
