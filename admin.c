a953800a9bea355824c93b71f74c83a183a59c6a
Satyendra Banjare <satyendrabanjare01@gmail.com>
2017-08-23 23:33:56 +0530

    Update admin.c

--- admin.c
+++ admin.c
@@ -1,1 +1,27 @@
 /* call main function of Main.hs and pass port number and channel name */
+
+
+#include <HsFFI.h>
+#ifdef __GLASGOW_HASKELL__
+// #include "main.h"
+extern void __stginit_Safe(void);
+#endif
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    hs_init(&argc, &argv);
+#ifdef __GLASGOW_HASKELL__
+    hs_add_root(__stginit_Safe);
+#endif
+    hs_exit();
+   
+  
+  
+  return 0;
+}
+
+
+
+
